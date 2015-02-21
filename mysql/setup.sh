ceea4ea0b2327198e9090de4978163d01c2474e6
David Lefever <dl@taktik.be>
2015-02-21 23:09:19 +0100

    Added mysql_backup.sh and mysql_restore.sh to mysql commons.

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+MYSQL_SCRIPT_PATH=`dirname "${BASH_SOURCE[0]}"`
+
 apt-get update
 apt-get -qq install mysql-server mysql-client
 
 sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
 
 mkdir -p /var/log/mysql
+
+cp $MYSQL_SCRIPT_PATH/scripts/mysql_backup.sh /usr/local/bin/
+cp $MYSQL_SCRIPT_PATH/scripts/mysql_restore.sh /usr/local/bin/
+chmod +x /usr/local/bin/mysql_*.sh
