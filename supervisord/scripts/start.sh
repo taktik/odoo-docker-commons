177997a43322d72cbb1a93f631d8d4d99e7a8e7c
David Lefever <dl@taktik.be>
2015-02-24 09:00:57 +0100

    Improved default supervisord start.sh script (removed possibility to override default CMD as the script will directly be ran with CMD).

--- supervisord/scripts/start.sh
+++ supervisord/scripts/start.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# If CMD was overriden, execute directly the command (e.g. /bin/bash)
-if [ "$#" -gt 0 ]; then
-    "$@"
-else
-    # Create log subdirectories if needed (if folder shared between host and container)
-    mkdir -p /var/log/mysql
-    mkdir -p /var/log/nginx
+# Create log subdirectories if needed (if folder shared between host and container)
+mkdir -p /var/log/mysql
+mkdir -p /var/log/nginx
 
-    # start all the services
-    exec /usr/local/bin/supervisord -n
-fi
+# start all the services
+exec /usr/local/bin/supervisord -n
