cc148e7544707df0490e19df20bcb428f6828f33
David Lefever <dl@taktik.be>
2015-07-09 09:01:11 +0200

    Improved pgsql pgCreateRole.sh script (add created user to openerp role), modified pg_hba.conf to allow all members of openerp role with md5.

--- pgsql/scripts/pgCreateRole.sh
+++ pgsql/scripts/pgCreateRole.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Create the specified role and add it to the openerp role
 
 if [[ ! ($1 && $2) ]]
 then
@@ -6,4 +7,5 @@ then
 fi
 
 psql -U postgres -c "CREATE USER \"$1\" WITH CREATEDB PASSWORD '$2';"
+psql -U postgres -c "GRANT openerp TO \"$1\";"
 echo "User $1 created"
