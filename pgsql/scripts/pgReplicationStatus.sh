a7aff37ef178ad7af21c71c3f48a869609fc9199
David Lefever <dl@taktik.be>
2015-08-07 19:20:27 +0200

    [IMP] Improved pgReplicationStatus.sh script.

--- pgsql/scripts/pgReplicationStatus.sh
+++ pgsql/scripts/pgReplicationStatus.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+source /root/.profile
 
-psql -c "select pg_last_xact_replay_timestamp() AS last_replay_timestamp, now() - pg_last_xact_replay_timestamp() AS replication_delay, pg_last_xlog_receive_location(), pg_last_xlog_replay_location();"
+psql -X -t --quiet --single-transaction --no-align -c "select coalesce(round(extract(epoch from (now() - pg_last_xact_replay_timestamp()))),0) AS time_lag, pg_last_xact_replay_timestamp() AS last_replay_timestamp, pg_last_xlog_receive_location() as last_receive_location, pg_last_xlog_replay_location() as last_replay_location;" | while IFS='|' read time_lag last_replay_timestamp last_receive_location last_replay_location; do
+    echo "`date -u +'%Y-%m-%dT%H:%M:%SZ'` '$time_lag' '$last_replay_timestamp' '$last_receive_location' '$last_replay_location'"
+done
