f8f1cbac10ce4f7cae25d770035cb4b5bdb7729e
David Lefever <dl@taktik.be>
2015-08-28 20:45:42 +0200

    [IMP] ERP-214 upgraded etcd version from 0.4.6 to 2.1.2.

--- etcd/setup.sh
+++ etcd/setup.sh
@@ -3,7 +3,8 @@
 SCRIPT_PATH=`dirname "${BASH_SOURCE[0]}"`
 
 # etcd
-curl -L  https://github.com/coreos/etcd/releases/download/v0.4.6/etcd-v0.4.6-linux-amd64.tar.gz -o /tmp/etcd.tar.gz
+#curl -L  https://github.com/coreos/etcd/releases/download/v0.4.6/etcd-v0.4.6-linux-amd64.tar.gz -o /tmp/etcd.tar.gz
+curl -L https://github.com/coreos/etcd/releases/download/v2.1.2/etcd-v2.1.2-linux-amd64.tar.gz -o /tmp/etcd.tar.gz
 tar -xzvf /tmp/etcd.tar.gz -C /tmp/
 mv /tmp/etcd-*/etcdctl /usr/local/bin/
 mv /tmp/etcd-*/etcd /usr/local/bin/
